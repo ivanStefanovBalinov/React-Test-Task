5e53159a12dad55c3267ff2a85138d60751dae43
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 19:32:47 +0300

    Update data.js

--- src/components/Sections/data.js
+++ src/components/Sections/data.js
@@ -1,28 +1,28 @@
 export const sectionData = [
     {
         title:'Brief',
-        paragraph:<> Complete <strong>brief writing or simple guidance</strong> on whjat to include weve got you covered.</>,
+        paragraph:<> Complete <strong>brief writing or simple guidance</strong> on what to include we've got you covered.</>,
         img:'https://cdn-icons-png.flaticon.com/512/2823/2823608.png',
         width:'420px',
-        opasity:'0.4',
+        opacity:'0.4',
         id:'1'
     },
     {
         title: 'Search',
         paragraph: <>In-depth agency search covering; <strong>criteria matching</strong>,
-        door knocking and due-delligence vetting.</>,
+        door knocking and due-diligence vetting.</>,
         img:'https://cdn-icons-png.flaticon.com/512/4363/4363878.png',
         width:'450px',
-        opasity:'0.7',
+        opacity:'0.7',
         id: '2'
     },
     {
         title: 'Pitch',
-        paragraph: <>Comprehensive <strong>pitch menagement</strong>,
+        paragraph: <>Comprehensive <strong>pitch management</strong>,
         including comms, diary management and pitch hosting.</>,
         img:'https://cdn-icons-png.flaticon.com/512/9586/9586260.png',
         width:'480px',
-        opasity:'1',
+        opacity:'1',
         id:'3'
     }
 ]
