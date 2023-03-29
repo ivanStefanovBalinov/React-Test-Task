3ef37353ae1ce9ae124443920a6f7bb78a331655
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 17:26:47 +0300

    Create index.js

---
+++ src/components/Typography/index.js
@@ -0,0 +1,16 @@
+import { SectionBigHeading } from "./SectionBigHeading";
+import { SectionHeading } from "./SectionHeading";
+import { SubHeading } from "./SectionSubheading";
+import { SectionTinyHeading } from "./SectionTinyHeading";
+
+
+const HeaderSection = () => {
+return(
+    <>
+        <SectionBigHeading text={'Manage agency selection'}/>
+        <SubHeading text={'Stengthen your onboarding process'}/>
+    </>
+)
+}
+
+export default HeaderSection
