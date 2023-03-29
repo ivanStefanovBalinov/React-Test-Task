0c13373c6f37bc1018dc113c91b86ba3a46cfe4a
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 17:10:52 +0300

    Update index.js

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,9 +1,4 @@
 import { MainWrapper } from "./MainWrapper";
-import { SectionContainer } from "./SectionContainer";
-import {sectionData} from "./data.js";
-
-const data = sectionData
-
 
 const testVideo = '/images/video.png'
 const Section = () => {
