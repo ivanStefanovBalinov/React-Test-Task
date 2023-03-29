403ca454aa87d2b1424b919707361afce9bfdbcf
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 17:18:15 +0300

    Update MainWrapper.jsx
    Add useState, onClickHandler function and two activeStyleFunction for changing border and innder header style on click.

--- src/components/Sections/MainWrapper.jsx
+++ src/components/Sections/MainWrapper.jsx
@@ -1,26 +1,59 @@
 import {StyledMainWrapper} from './sectionStyling'
 import { SectionContainer } from './SectionContainer'
 import {sectionData} from "./data.js";
+import { useState } from 'react';
+
 
 
 
 export const MainWrapper  = ({img}) => {
-    const data = sectionData
-
+    const data = sectionData;
     
-    return <StyledMainWrapper className='main-wrapper'>
+    const [appState, setAppState] = useState({
+        active: null,
+        objects: data
+    })
+    //Click handler for onClick event, hooked on SectionContainer.
+    const clickHandler = (index) => {
+        setAppState({...appState, active: appState.objects[index] })
+        
+    }
+    //Function for add or remove class that add border to SectionContainer.
+    const activeStyleBorder = (index) => {
+        if(appState.objects[index] === appState.active){
+            return 'onClick-border-style' 
+        } else {
+            return ''
+        }
+    }
+    //Function that change styling of InnerHeader by add or remove class.
+    const activeStyleInnerHeader = (index) => {
+        if(appState.objects[index] === appState.active){
+            return 'onClick-header-style' 
+        } else {
+            return ''
+        } 
+    } 
+    
+    return <StyledMainWrapper className={'main-wrapper'}>
         <div>
             <img className='fake-video' src={img}/>
         </div>
         <div className='section-wrapper'>
-            {data.map((obj) => {
-                const {img, title, paragraph, width, opasity} = obj
-                return <SectionContainer 
-                img={img} 
-                title={title} 
-                paragraph={paragraph}
-                width={width}
-                opasity={opasity}/>
+            {appState.objects.map((obj, index) => {
+                const {img, title, paragraph, width, opasity,id} = obj
+                return <SectionContainer
+                    
+                    id = {id} 
+                    img={img} 
+                    title={title} 
+                    paragraph={paragraph}
+                    width={width}
+                    opasity={opasity}
+                    onClick={() => clickHandler(index)}
+                    classNameMainDiv = {activeStyleBorder(index)}
+                    classNameTitle = {activeStyleInnerHeader(index)}
+                />
             })}
         </div>
     </StyledMainWrapper>
