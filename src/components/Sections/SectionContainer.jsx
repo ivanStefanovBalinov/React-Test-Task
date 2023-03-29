ce121f7f1c3d7e7d7114799db5740e9c8bd7106e
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 17:12:26 +0300

    Update SectionContainer.jsx

--- src/components/Sections/SectionContainer.jsx
+++ src/components/Sections/SectionContainer.jsx
@@ -2,17 +2,22 @@ import { StyledSectionContainer } from "./sectionStyling";
 import { SectionInnerHeading } from "../Typography/SectionInnerHeading";
 import SectionParagraph from "../Typography/SectionParagraph";
 
-
-export const SectionContainer = ({img, title, paragraph,  width, opasity}) => {
-    
-    return <StyledSectionContainer 
-        style={{width:`${width}`, 
-        backgroundColor:`rgba(241, 241, 241, ${opasity})`}} 
-        className="section-text-wrapper" >
-        <div className="section-img-wrapper"><img className="section-img" src={img} alt={title}/></div>
-        <div>
-            <SectionInnerHeading title={title}></SectionInnerHeading>
-            <SectionParagraph paragraph={paragraph}></SectionParagraph>
-        </div>
+export const SectionContainer = ({ img, title, paragraph, width, opasity, onClick, classNameMainDiv, classNameTitle }) => {
+  return (
+    <StyledSectionContainer onClick={onClick}
+      style={{
+        width: `${width}`,
+        backgroundColor: `rgba(241, 241, 241, ${opasity})`,
+      }}
+      className={classNameMainDiv}
+    >
+      <div>
+        <img className="section-img" src={img} alt={title} />
+      </div>
+      <div>
+        <SectionInnerHeading className={classNameTitle} title={title}></SectionInnerHeading>
+        <SectionParagraph paragraph={paragraph}></SectionParagraph>
+      </div>
     </StyledSectionContainer>
-}
+  );
+};
