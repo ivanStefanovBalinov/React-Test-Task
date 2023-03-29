7848f0d5ed96b075b8233b0becf085dca7766011
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-03-29 21:34:36 +0300

    Update SectionContainer.jsx

--- src/components/Sections/SectionContainer.jsx
+++ src/components/Sections/SectionContainer.jsx
@@ -2,12 +2,12 @@ import { StyledSectionContainer } from "./elements";
 import { SectionInnerHeading } from "../Typography/SectionInnerHeading";
 import SectionParagraph from "../Typography/SectionParagraph";
 
-export const SectionContainer = ({ img, title, paragraph, width, opasity, onClick, classNameMainDiv, classNameTitle }) => {
+export const SectionContainer = ({ img, title, paragraph, width, opacity, onClick, classNameMainDiv, classNameTitle }) => {
   return (
     <StyledSectionContainer onClick={onClick}
       style={{
         width: `${width}`,
-        backgroundColor: `rgba(241, 241, 241, ${opasity})`,
+        backgroundColor: `rgba(241, 241, 241, ${opacity})`,
       }}
       className={classNameMainDiv}
     >
